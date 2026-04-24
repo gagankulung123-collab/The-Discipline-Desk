1cf7219a953943b4f3301732afbf99625b3b7911
Gagan Rai <gagankulung123@gmail.com>
2026-04-24 14:35:08 +0545

    Printed the main menu

--- main.c
+++ main.c
@@ -5,5 +5,14 @@ int main()
     printf("-- THE DISCIPLINE DESK --\n");
     printf("=========================\n");
     printf("Welcome to your productivity hub!\n");
+    
+    printf("\n-- Main Menu --\n");
+    printf("---------------\n");
+    printf("1. Add Tasks\n");
+    printf("2. View Tasks\n");
+    printf("3. Search Tasks\n");
+    printf("4. Progress Report\n");
+    printf("5. Motivation Quote\n");
+    printf("6. Exit\n");
     return 0;
 }
