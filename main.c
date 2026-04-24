c9ba35d5bc388beaee825694952bade9780f20fc
Gagan Rai <gagankulung123@gmail.com>
2026-04-24 14:00:06 +0545

    Renamed the codes.c to main.c and printed the project title

---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("=========================\n");
+    printf("-- THE DISCIPLINE DESK --\n");
+    printf("=========================\n");
+    printf("Welcome to your productivity hub!\n");
+    return 0;
+}
